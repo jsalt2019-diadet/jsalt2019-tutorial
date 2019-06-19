3e812cfe5a1dc552bb1cf95d4d5629af8549f437
Saurabh Kataria <skataria@master>
2019-06-19 01:14:11 +0000

    sitw_tutorial testing done on AWS

---
+++ make_aws_links.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+linktomake=tools/kaldi/kaldi
+if [ ! -L $linktomake ]; then
+    ln -sv /export/fs01/jtrmal/kaldi $linktomake
+fi
+
+linktomake=tools/hyperion
+if [ ! -L $linktomake ]; then
+    ln -sv /export/fs01/skataria/hyperion $linktomake
+fi
+
+mkdir -pv egs/sitw_tutorial/v1/exp
+linktomake=egs/sitw_tutorial/v1/exp/xvector_nnet_2a.1.voxceleb_div2
+if [ ! -L $linktomake ]; then
+    ln -sv /export/fs01/skataria/storage/xvector_nnet_2a.1.voxceleb_div2 $linktomake
+fi
+
+linktomake=tools/anaconda/anaconda3.5
+if [ ! -L $linktomake ]; then
+    ln -sv /home/skataria/anaconda3 $linktomake
+fi
